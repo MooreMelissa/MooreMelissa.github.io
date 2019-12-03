9f9351a84074796f44ef14c52eceb9ee04610d90
Melissa Moore <melissamoore@Melissas-MacBook.local>
2019-12-03 13:09:16 -0600

    fix code

--- javascript/final.js
+++ javascript/final.js
@@ -61,6 +61,59 @@ function resetBoard() {
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+
+// reveal/hide side menu
+function toggleNav() {
+    let updateElement = document.getElementById("menu-icon");
+    // toggle adds or removes a class attribute accordingly (the class name is used by CSS)
+    updateElement.classList.toggle("open");
+}
+
 // stuff for form
 // add an event listener for form submission
 document.getElementById('registration')
+
+// collect the user inputs, store them locally
+function store() {
+
+    //retrieve user inputs
+    var fname = document.getElementById('first-name').value,
+        lname = document.getElementById('last-name').value,
+        age = document.getElementById('age').value,
+        email = document.getElementById('email').value,
+        phone = document.getElementById('phone').value,
+        experience = document.getElementById('experience').value,
+        message = document.getElementById('message').value;
+
+    // create associative array
+    var assarr = { "firstname": fname, "lastname": lname, "age": age, "email": email, "phone": phone, "experience": experience, "message": message};
+
+    //save the javascript entities in the local storage as JSON strings
+    localStorage.setItem("assarrValues", JSON.stringify(assarr));
+
+    console.log (localStorage.getItem("assarrValues"));
+
+    // parse the JSON string into javascript associative array
+    var localAssarr = JSON.parse( localStorage.getItem("assarrValues"));
+
+
+    document.getElementById("output1").value = localAssarr["first-name"] + ", thank you for your registration.";
+    document.getElementById("output2").value = "Miss Melissa will be contacting you shortly by the email address of " + localAssarr["email"];
+
+
+    refresh();
+
+    // retrieve values from the items stored in the local storage
+
+    // parse the JSON string into javascript associative array
+    var localAssarr = JSON.parse( localStorage.getItem("assarrValues"));
+
+    var out = document.getElementById("output");
+    document.getElementById("output").value = localAssarr["first-name"] + ", thank you for your registration.";
+    document.getElementById("output").value = "Miss Melissa will be contacting you shortly by the email address of " + localAssarr["email"];
+
+
+
+}
+
+
